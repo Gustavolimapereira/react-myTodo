abbdb7f05d24e9cf3913a8f4ce92ffbde74e9d8e
gustavolimapereira <gu.lim@hotmail.com>
2025-09-17 09:01:12 -0300

    inserido o componente de icone

--- src/pages/page-components.tsx
+++ src/pages/page-components.tsx
@@ -1,7 +1,16 @@
+import Icon from "../components/icon";
 import Text from "../components/text";
+import TrashIcon from "../assets/icons/trash.svg?react"
 
 export default function PageComponents() {
     return (
+        <>
         <Text variant='body-sm-bold' className="text-pink-base">Olá Mundo</Text>
+
+        <div className="flex gap-1">
+            <Icon svg={TrashIcon} className="fill-green-base" />
+        </div>
+        </>
+        
     )
 }
