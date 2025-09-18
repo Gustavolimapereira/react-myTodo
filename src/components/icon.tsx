fd2e1bd5dda36a2bee3bccb9aeabe97f43b348ff
gustavolimapereira <gu.lim@hotmail.com>
2025-09-18 10:57:58 -0300

    update ajuste css

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,19 +1,30 @@
-import type { VariantProps } from "class-variance-authority";
-import type React from "react";
-import { iconVariants } from "../styles/iconVariants";
+import { cva, type VariantProps } from "class-variance-authority";
 
+export const iconVariants = cva("", {
+  variants: {
+    animate: {
+      true: "animate-spin",
+      false: "",
+    },
+  },
+  defaultVariants: {
+    animate: false,
+  },
+});
 
-interface IconProps extends React.ComponentProps<'svg'>,
-VariantProps<typeof iconVariants> {
-    svg: React.FC<React.ComponentProps<'svg'>>
+interface IconProps
+  extends React.ComponentProps<"svg">,
+    VariantProps<typeof iconVariants> {
+  svg: React.FC<React.ComponentProps<"svg">>;
 }
 
-export default function Icon ({
-    svg: SvgComponent,
-    animate,
-    className,
-    ...props
+export default function Icon({
+  svg: SvgComponent,
+  animate,
+  className,
+  ...props
 }: IconProps) {
-    return (<SvgComponent className={iconVariants({animate, className})} {...props} />
-); 
+  return (
+    <SvgComponent className={iconVariants({ animate, className })} {...props} />
+  );
 }
--- src/pages/page-components.tsx
+++ src/pages/page-components.tsx
@@ -1,16 +1,17 @@
 import Icon from "../components/icon";
 import Text from "../components/text";
-import TrashIcon from "../assets/icons/trash.svg?react"
+import TrashIcon from "../assets/icons/trash.svg?react";
 
 export default function PageComponents() {
-    return (
-        <>
-        <Text variant='body-sm-bold' className="text-pink-base">Olá Mundo</Text>
+  return (
+    <>
+      <Text variant="body-sm-bold" className="text-pink-base">
+        Olá Mundo
+      </Text>
 
-        <div className="flex gap-1">
-            <Icon svg={TrashIcon} className="fill-green-base" />
-        </div>
-        </>
-        
-    )
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+      </div>
+    </>
+  );
 }
